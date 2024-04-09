6159b69b116ff0bf7168e3045b176b783a9a1166
yash choraria <yc21chb0a60@student.nitw.ac.in>
2024-04-10 03:39:45 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const searchbar = document.querySelector(".search-bar-container");
+const profilecontainer = document.querySelector(".profile-container");
+const root = document.documentElement.style;
+const get = (param)=>document.getElementById(`${param}`);
+const url = ""
